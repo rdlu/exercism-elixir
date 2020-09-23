c5f2224bb80e0b4ccbe61fd11339b0f96edea94c
Rodrigo Dlugokenski <rdlu.one@gmail.com>
2020-09-23 14:43:54 -0300

    coin change: using an easy pattern

--- change/lib/change.ex
+++ change/lib/change.ex
@@ -16,6 +16,30 @@ defmodule Change do
   """
 
   @spec generate(list, integer) :: {:ok, list} | {:error, String.t()}
+  def generate(_, 0), do: {:ok, []}
+  def generate(_, target) when target < 0, do: {:error, "cannot change"}
   def generate(coins, target) do
+    best_candidate = Enum.find_value(1..(div(target, Enum.min(coins)) + 1),
+      fn i -> find_combinations_sum(coins, i, target) end)
+
+    if best_candidate do
+      {:ok, best_candidate}
+    else
+      {:error, "cannot change"}
+    end
+  end
+
+  @spec find_combinations_sum(list, integer, integer) :: list | nil
+  def find_combinations_sum(coins, chunk_size, target) do
+    Enum.find(combination_with_repetition(coins, chunk_size),
+      fn candidate -> Enum.sum(candidate) == target end)
+  end
+
+
+  @spec combination_with_repetition(list, integer) :: list
+  def combination_with_repetition(_, 0), do: [[]]
+  def combination_with_repetition([], _), do: []
+  def combination_with_repetition([h|t]=s, n) do
+    (for l <- combination_with_repetition(s, n-1), do: [h|l]) ++ combination_with_repetition(t, n)
   end
 end
