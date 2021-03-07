39d82f6b11c402a9c2e7a99be3300162bf12f0a8
Rodrigo Dlugokenski <rddweb@gmail.com>
2021-03-07 18:40:19 -0300

    RNA

--- rna-transcription/lib/rna_transcription.ex
+++ rna-transcription/lib/rna_transcription.ex
@@ -9,5 +9,12 @@ defmodule RnaTranscription do
   """
   @spec to_rna([char]) :: [char]
   def to_rna(dna) do
+    dna |>
+      Enum.map(&char_to_rna(&1))
   end
+
+  def char_to_rna(?A) do ?U end
+  def char_to_rna(?C) do ?G end
+  def char_to_rna(?G) do ?C end
+  def char_to_rna(?T) do ?A end
 end
