550f713fe0ac3eda94d53f6a40487ab3c99022f5
Rodrigo Dlugokenski <rodrigo.git@dlu.one>
2022-06-15 14:41:43 -0300

    secrets: using shorthands

--- secrets/lib/secrets.ex
+++ secrets/lib/secrets.ex
@@ -1,17 +1,17 @@
 defmodule Secrets do
   def secret_add(secret), do: fn p -> p + secret end
 
-  def secret_subtract(secret), do: fn p -> p - secret end
+  def secret_subtract(secret), do: &(&1 - secret)
 
   def secret_multiply(secret), do: fn p -> p * secret end
 
-  def secret_divide(secret), do: fn p -> div(p, secret) end
+  def secret_divide(secret), do: &(div &1, secret)
 
   def secret_and(secret), do: fn p -> Bitwise.band(p, secret) end
 
-  def secret_xor(secret), do: fn p -> Bitwise.bxor(p, secret) end
+  def secret_xor(secret), do: &(Bitwise.bxor &1, secret)
 
   def secret_combine(secret_function1, secret_function2) do
-    fn p -> secret_function2.(secret_function1.(p)) end
+    fn p -> p |> secret_function1.() |> secret_function2.() end
   end
 end
