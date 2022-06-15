9ca0b42b7525b9893ca7c34971ff6a4fc4ee7ab9
Rodrigo Dlugokenski <rodrigo.git@dlu.one>
2022-06-15 15:25:46 -0300

    freelancer rates: using shorthand and instance vars

--- freelancer-rates/lib/freelancer_rates.ex
+++ freelancer-rates/lib/freelancer_rates.ex
@@ -1,19 +1,17 @@
 defmodule FreelancerRates do
-  def daily_rate(hourly_rate) do
-    hours_per_day = 8.0
-    hourly_rate * hours_per_day
-  end
+  @hours_per_day 8.0
+  @days_per_month 22
+
+  def daily_rate(hourly_rate), do: hourly_rate * @hours_per_day
 
   def apply_discount(before_discount, discount) do
     before_discount * (1 - discount / 100)
   end
 
   def monthly_rate(hourly_rate, discount) do
-    days_per_month = 22
-    daily_rate(hourly_rate) * days_per_month
+    daily_rate(hourly_rate) * @days_per_month
      |> apply_discount(discount)
      |> ceil
-     |> trunc
   end
 
   def days_in_budget(budget, hourly_rate, discount) do
