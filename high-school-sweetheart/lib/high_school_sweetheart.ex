4f3555a6295610d4c056e9eb787469e577a5e771
Rodrigo Dlugokenski <rodrigo.git@dlu.one>
2022-06-17 16:17:25 -0300

    High School: String manipulation, pipes

---
+++ high-school-sweetheart/lib/high_school_sweetheart.ex
@@ -0,0 +1,45 @@
+defmodule HighSchoolSweetheart do
+  def first_letter(name) do
+    name
+    |> String.trim
+    |> String.first
+  end
+
+  def initial(name) do
+    "#{clean_initial(name)}."
+  end
+
+  defp clean_initial(name) do
+    name
+    |> first_letter
+    |> String.upcase
+  end
+
+  def initials(full_name) do
+    full_name
+    |> String.split(" ")
+    |> Enum.map(&initial/1)
+    |> Enum.join(" ")
+  end
+
+  def pair(full_name1, full_name2) do
+    n1 = initials(full_name1)
+    n2 = initials(full_name2)
+    """
+         ******       ******
+       **      **   **      **
+     **         ** **         **
+    **            *            **
+    **                         **
+    **     #{n1}  +  #{n2}     **
+     **                       **
+       **                   **
+         **               **
+           **           **
+             **       **
+               **   **
+                 ***
+                  *
+    """
+  end
+end
