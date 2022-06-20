67a52328a370e323d44efcc74f9ad872d2cf5c9a
Rodrigo Dlugokenski <git@dlu.one>
2022-06-20 09:20:22 -0300

    Space Age

---
+++ space-age/lib/space_age.ex
@@ -0,0 +1,39 @@
+defmodule SpaceAge do
+  @type planet ::
+          :mercury
+          | :venus
+          | :earth
+          | :mars
+          | :jupiter
+          | :saturn
+          | :uranus
+          | :neptune
+
+  @orbital_periods %{
+    mercury: 0.2408467,
+    venus: 0.61519726,
+    earth: 1.0,
+    mars: 1.8808158,
+    jupiter: 11.862615,
+    saturn: 29.447498,
+    uranus: 84.016846,
+    neptune: 164.79132
+  }
+  @earth_year 31557600
+
+  @doc """
+  Return the number of years a person that has lived for 'seconds' seconds is
+  aged on 'planet', or an error if 'planet' is not a planet.
+  """
+  @spec age_on(planet, pos_integer) :: {:ok, float} | {:error, String.t()}
+  def age_on(planet, seconds) do
+    cond do
+      planet in Map.keys(@orbital_periods) -> {:ok, calc_age(planet, seconds)}
+      true -> {:error, "not a planet"}
+    end
+  end
+
+  defp calc_age(planet, seconds) do
+    seconds / @earth_year / @orbital_periods[planet]
+  end
+end
