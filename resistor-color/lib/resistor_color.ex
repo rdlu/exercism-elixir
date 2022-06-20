a04fd23a4f48b032d01135961e5fa98b658bedcf
Rodrigo Dlugokenski <git@dlu.one>
2022-06-20 09:44:12 -0300

    resistor color

---
+++ resistor-color/lib/resistor_color.ex
@@ -0,0 +1,9 @@
+defmodule ResistorColor do
+  @colors %{black: 0, brown: 1, red: 2, orange: 3, yellow: 4, green: 5,
+  blue: 6, violet: 7, grey: 8, white: 9}
+  @doc """
+  Return the value of a color band
+  """
+  @spec code(atom) :: integer()
+  def code(color) when is_map_key(@colors, color), do: @colors[color]
+end
