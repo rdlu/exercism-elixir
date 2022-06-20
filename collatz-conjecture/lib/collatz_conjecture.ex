5741e6ecf81e973cfd91a11bf8eae5e59d0a5cfb
Rodrigo Dlugokenski <git@dlu.one>
2022-06-20 08:58:30 -0300

    Collatz Conjecture: rec ver, avoid direct raise

--- collatz-conjecture/lib/collatz_conjecture.ex
+++ collatz-conjecture/lib/collatz_conjecture.ex
@@ -6,8 +6,7 @@ defmodule CollatzConjecture do
     - if number is even, divide by 2
   """
   @spec calc(i :: pos_integer()) :: non_neg_integer()
-  def calc(i) when i < 1, do: raise FunctionClauseError
   def calc(1), do: 0
-  def calc(i) when rem(i, 2) == 0, do: 1 + calc(div(i, 2))
-  def calc(i) when rem(i, 2) != 0, do: 1 + calc(i * 3 + 1)
+  def calc(i) when rem(i, 2) == 0 and i > 0, do: 1 + calc(div(i, 2))
+  def calc(i) when rem(i, 2) != 0 and i > 0, do: 1 + calc(i * 3 + 1)
 end
