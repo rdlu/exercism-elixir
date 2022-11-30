6ad7a23fbcc5d8768b982fb299a4116086ea1313
Rodrigo Dlugokenski <git@dlu.one>
2022-11-30 10:01:38 -0300

    anon functions: strain with recursion

--- strain/lib/strain.ex
+++ strain/lib/strain.ex
@@ -6,9 +6,10 @@ defmodule Strain do
   Do not use `Enum.filter`.
   """
   @spec keep(list :: list(any), fun :: (any -> boolean)) :: list(any)
-  def keep(list, fun) do
-    for item <- list, fun.(item), do: item
-  end
+  def keep([], _), do: []
+
+  def keep([head | tail], fun),
+    do: if(fun.(head), do: [head | keep(tail, fun)], else: keep(tail, fun))
 
   @doc """
   Given a `list` of items and a function `fun`, return the list of items where
@@ -17,7 +18,8 @@ defmodule Strain do
   Do not use `Enum.reject`.
   """
   @spec discard(list :: list(any), fun :: (any -> boolean)) :: list(any)
-  def discard(list, fun) do
-    for item <- list, !fun.(item), do: item
-  end
+  def discard([], _), do: []
+
+  def discard([head | tail], fun),
+    do: if(fun.(head), do: discard(tail, fun), else: [head | discard(tail, fun)])
 end
