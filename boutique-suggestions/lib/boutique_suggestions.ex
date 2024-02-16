acb4e37068fd8eb1612c454091e97d5b348dae30
Rodrigo Dlugokenski <git@dlu.one>
2024-02-16 18:53:04 -0300

    boutique: list comprehensions using map shorthands

--- boutique-suggestions/lib/boutique_suggestions.ex
+++ boutique-suggestions/lib/boutique_suggestions.ex
@@ -1,18 +1,12 @@
 defmodule BoutiqueSuggestions do
   def get_combinations(tops, bottoms, options \\ []) do
+    target_price = Keyword.get(options, :maximum_price, 100)
+
     for top <- tops,
         bottom <- bottoms,
-        map_dif(top, bottom, :base_color) &&
-          map_comb_max(top, bottom, Keyword.get(options, :maximum_price, 100)) do
+        top.base_color != bottom.base_color &&
+          top.price + bottom.price <= target_price do
       {top, bottom}
     end
-  end
-
-  defp map_dif(map1, map2, property) do
-    Map.get(map1, property) != Map.get(map2, property)
-  end
-
-  defp map_comb_max(map1, map2, max) do
-    Map.get(map1, :price) + Map.get(map2, :price) <= max
   end
 end
